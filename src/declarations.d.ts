a28f5ae57d8ca436cf727bab7683600b0681a3f5
priska <priskakohnen@gmail.com>
2026-03-15 10:25:55 +0100

    move imges

---
+++ src/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module "*.jpg";
+declare module "*.JPG";
+declare module "*.jpeg";
+declare module "*.png";
+declare module "*.mp3";
+declare module "*.mp4";
+declare module "*.m4a";
+declare module "*.txt";
